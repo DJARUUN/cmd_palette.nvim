a73d873794e75966bafc3ef2a5effd0cc3ab5a85
djaruun <caargajac@gmail.com>
2024-03-31 22:12:58 +0200

    .

--- lua/telescope/_extensions/cmd_palette.lua
+++ lua/telescope/_extensions/cmd_palette.lua
@@ -5,7 +5,7 @@ local function run(opts)
     init.show_palette(opts)
 end
 
-run(require("telescope.themes").palette{})
+-- run(require("telescope.themes").palette{})
 
 return require("telescope").register_extension({
     setup = setup,
