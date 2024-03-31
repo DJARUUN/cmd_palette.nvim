efa5f08b67ef15efcf4d63b894e0d72cd1ccba9b
djaruun <caargajac@gmail.com>
2024-03-31 22:40:56 +0200

    .

--- lua/telescope/_extensions/cmd_palette.lua
+++ lua/telescope/_extensions/cmd_palette.lua
@@ -1,23 +1,20 @@
 local has_telescope, telescope = pcall(require, "telescope")
 
 if not has_telescope then
-    return
+    error("cmd_palette.nvim requires telescope.nvim - https://github.com/nvim-telescope/telescope.nvim")
 end
 
 package.loaded["lua.cmd_palette.init"] = nil
 local init = require("lua.cmd_palette.init")
 
-local function run(opts)
+local run = function(opts)
     init.show_palette(opts)
 end
-
--- run(require("telescope.themes").palette{})
 
 return telescope.register_extension({
     setup = function(ext_config, config) end,
     exports = {
-        ["cmd_palette"] = run,
-        fortnite = run,
+        cmd_palette = run,
     }
 })
 
